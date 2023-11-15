603fa4cbf936a2bcff836bece7c9d6a521f5699e
Marvin Lee Kobert <marvin.kobert@me.com>
2023-11-15 20:43:46 +0100

    Initial Commit

---
+++ NavigationPath/ContentView.swift
@@ -0,0 +1,22 @@
+//  Created by Marvin Lee Kobert on 15.11.23.
+//  
+//
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NavigationPath/NavigationPathApp.swift
@@ -0,0 +1,15 @@
+//  Created by Marvin Lee Kobert on 15.11.23.
+//  
+//
+
+
+import SwiftUI
+
+@main
+struct NavigationPathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
